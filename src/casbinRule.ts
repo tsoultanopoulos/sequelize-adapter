1b11726028336a2a1760f3a6db8c21ef1a3e9f30
tsoultanopoulos <tsoultanopoulos@wings-ict-solutions.eu>
2023-06-23 12:27:01 +0300

    Fixing multitentancy issue where create different instances of Sequelize adapter

--- src/casbinRule.ts
+++ src/casbinRule.ts
@@ -1,16 +1,4 @@
-// Copyright 2018 The Casbin Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// casbinRule.ts (library)
 
 import {
   Table,
@@ -44,12 +32,16 @@ export class CasbinRule extends Model<CasbinRule> {
   public v5: string;
 }
 
-export function updateCasbinRule(
+export function createCasbinRule(
   tableName = 'casbin_rule',
   schema?: string
-): void {
-  const options = getOptions(CasbinRule.prototype);
+): typeof CasbinRule {
+  class CustomCasbinRule extends CasbinRule {}
+
+  const options = getOptions(CustomCasbinRule.prototype);
   options!.tableName = tableName;
   options!.schema = schema;
-  setOptions(CasbinRule.prototype, options!);
+  setOptions(CustomCasbinRule.prototype, options!);
+
+  return CustomCasbinRule;
 }
